3d06947feb9604312caf9df89f02a337a4e73a8c
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-08-13 12:56:12 -0700

    Add username attribute to User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :username, :email, :password, :password_confirmation, :remember_me ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [ :login, :password ]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_12_190110) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_174723) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,7 +19,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_12_190110) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
